7305328d68c318232662dfa1c0affacc90089cfb
priti-dt <103584553+priti-dt@users.noreply.github.com>
2022-07-14 13:46:10 +0530

    category edit page

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -17,17 +17,14 @@
             ];            
             //Add in popup
             let actionsSettings = {add:"popup",popupMid:'createcategory',import:1,export:1,hideFromExport:[1]};
-            let hideFromExport = [1];//give column index to hide from export
+            let hideFromExport = [1];//give column index to hide from export        
+            var routesstore = "{{ route('category.store') }}";
         </script>
-    </x-slot>
-
-    <x-slot name="include_admin_script">
-        <script>
-         var routesstore = "{{ route('category.store') }}";
-        </script>
+        
         <script type="module" src="{{asset('js/libraries/jquery-validate1.19.3/jquery.validate.min.js')}}"></script>
         <script type="module" src="{{asset('js/libraries/jquery-validate1.19.3/additional-methods.min.js')}}"></script>
         <script type="module" src="{{ URL::asset('js/admin/category/index.js') }}"></script>
+
     </x-slot>
   
 </x-app-layout>
